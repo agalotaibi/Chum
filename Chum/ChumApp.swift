7f70a518657755232e4bf0ed800a373b7b6a6277
Amaal Al Otaibi <hopes.gmo@gmail.com>
2022-12-07 14:42:05 +0300

    Initial Commit

---
+++ Chum/ChumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChumApp.swift
+//  Chum
+//
+//  Created by Amaal on 13/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct ChumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
